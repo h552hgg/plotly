3d8c54e5bdf17c9b5942ae0b62be02e9c78788e0
h552hgg <h552hgg@gmail.com>
2021-01-29 13:08:26 -0600

    bubble chart done

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,44 @@
 
 
 
+
+
 d3.json("samples.json").then((importedData) => {
     // console.log(importedData)
+    // var names = Object.values(importedData.names);
+    // ///////////////////////
+    // console.log(names)
+    // d3.selectAll("#selDataset").on("change", optionChanged);
+
+    // // function updatePlotly(newdata){
+    // //     var id =document.getElementById("sample-metadata");
+    // //     Plotly.restyle(id,"values",[newdata]);
+    // // }
+    // function optionChanged(dataset) {
+    //     for (var i = 0; i < names.length; i++);
+    //     var dataset = names[i];
+    //     // updatePlotly(data)
+
+    // }
+
+
+
+
+
+
+
+
+    //////////////////////////////////////
+
+
+
+
     data = importedData.samples
     // console.log(data)
 
 
     var sortedValues = data.sort((a, b) => b.sample_values - a.sample_values);
-    console.log(sortedValues)
+    // console.log(sortedValues)
 
     var sample940 = sortedValues.filter(obj => obj.id === `940`)[0];
 
@@ -47,7 +77,15 @@ d3.json("samples.json").then((importedData) => {
     var trace2 = {
         x: sample940.otu_ids.map(id => ` ${id}`),
         y: sample940.sample_values.slice(0, 10),
+        text: sample940.otu_labels,
         mode: 'markers',
+        marker: {
+            color: sample940.otu_ids,
+            size: sample940.sample_values,
+            opacity: [0.6, 0.7, 0.8, 0.9]
+        },
+
+
 
 
     };
