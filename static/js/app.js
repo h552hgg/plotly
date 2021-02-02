0712a2fb17cd103e78bcc828a8291bb70e149b2b
h552hgg <h552hgg@gmail.com>
2021-02-01 22:00:17 -0600

    preload added

--- static/js/app.js
+++ static/js/app.js
@@ -21,6 +21,7 @@ d3.json("samples.json").then((importedData) => {
     Object.entries(datanames).forEach(([key, value]) => {
         id_list.insert("option").text(value)
 
+
     });
 })
 
@@ -35,15 +36,25 @@ function demoBar(id_val) {
 
 
         const sample = dataid.filter(x => x.id === subjectId)[0];
+
         // console.log(sample)
 
         panel = d3.select(".panel-body")
         panel.html("")
-        Object.entries(sample).forEach(([key, value]) => {
+        let demoFill = Object.entries(sample).forEach(([key, value]) => {
             panel.append("p").text(`${key}: ${value}`)
+            // // console.log(key, value)
+
+            // if (key === "wfreq")
+            //     return value;
+            // console.log(value)
+
         });
+
         barGraph(id_val);
-        // bubbleGraph(id_val);
+
+
+
     });
 
 };
@@ -61,11 +72,6 @@ function barGraph(id) {
 
         var sortedValues = datasort.sort((a, b) => b.sample_values - a.sample_values);
         var sample940 = sortedValues.filter(obj => obj.id === id)[0];
-        // console.log(sample940)
-
-        // var dataSearch = dataid
-        // var freq = dataSearch
-        // console.log(freq)
 
 
 
@@ -85,7 +91,7 @@ function barGraph(id) {
 
 
         var layout = {
-            title: "Top OTU",
+            title: `Top OTU of ${id}`,
             margin: {
                 l: 100,
                 r: 100,
@@ -97,9 +103,6 @@ function barGraph(id) {
         Plotly.newPlot("bar", chartData, layout);
 
 
-
-
-        // function bubbleGraph(id) {
 
         var trace2 = {
             x: sample940.otu_ids.map(id => ` ${id}`),
@@ -151,17 +154,94 @@ function barGraph(id) {
 }
 
 
-// d3.json("samples.json").then((importedData) => {
-//     ///variables for different sections of the json file 
-//     // data = importedData.samples
-//     dataid = importedData.metadata
-//     // console.log(dataid)
-//     datanames = importedData.names
-//     // console.log(datanames)
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+////Preload graph////////
 
 
 
 
-// })
 
 
+
+
+d3.json("samples.json").then((data) => {
+
+    datasort = data.samples
+
+
+    datanames = data.names
+    // console.log(id)
+    // var subject = parseInt(id)
+    // console.log(subject)
+    dataid = data.metadata
+    var sample = dataid.filter(x => x.id === 940)[0];
+
+
+    panel = d3.select(".panel-body")
+
+    Object.entries(sample).forEach(([key, value]) => {
+        panel.append("p").text(`${key}: ${value}`)
+
+    });
+
+
+    var sortedValues = datasort.sort((a, b) => b.sample_values - a.sample_values);
+    var sample940 = sortedValues.filter(obj => obj.id === `940`)[0];
+    // console.log(sample940)
+
+    // var dataSearch = dataid
+    // var freq = dataSearch
+    // console.log(freq)
+
+
+
+
+
+    var trace1 = {
+        x: sample940.sample_values.slice(0, 10).reverse(),
+        y: sample940.otu_ids.map(id => `OTU ${id}`).slice(0, 10).reverse(),
+        text: sample940.otu_labels.slice(0, 10).reverse(),
+        name: "OTU",
+        type: "bar",
+        orientation: "h"
+    };
+
+
+    var chartData = [trace1];
+
+
+    var layout = {
+        title: "Top OTU",
+        margin: {
+            l: 100,
+            r: 100,
+            t: 100,
+            b: 100
+        }
+    };
+
+    Plotly.newPlot("bar", chartData, layout);
+
+
+
+    var trace2 = {
+        x: sample940.otu_ids.map(id => ` ${id}`),
+        y: sample940.sample_values.slice(0, 10),
+        text: sample940.otu_labels,
+        mode: 'markers',
+        marker: {
+            color: sample940.otu_ids,
+            size: sample940.sample_values,
+            opacity: [0.6, 0.7, 0.8, 0.9]
+        },
+
+
+
+
+    };
+
+    var dataChart = [trace2]
+
+
+    Plotly.newPlot("bubble", dataChart);
+});
